d43e82281887d8fedf7988442c7b2fa1f00c2526
Saif Mahmud <sikhlana@gmail.com>
2021-08-13 23:45:13 +0600

    Stops request if no request-id header is sent

--- src/main/java/name/saifmahmud/demo/http/filters/RequestIdFilter.java
+++ src/main/java/name/saifmahmud/demo/http/filters/RequestIdFilter.java
@@ -1,19 +1,38 @@
 package name.saifmahmud.demo.http.filters;
 
 import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
 public class RequestIdFilter implements Filter {
+    MessageSource messageSource;
+
+    @Autowired
+    RequestIdFilter(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
-        MDC.put("REQUEST_ID", req.getHeader("Request-Id"));
+        String requestId = req.getHeader("Request-Id");
 
+        if (requestId == null || requestId.equals("")) {
+            HttpServletResponse res = (HttpServletResponse) response;
+            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, messageSource.getMessage("exceptions.invalid_request_id", null, LocaleContextHolder.getLocale()));
+
+            return;
+        }
+
+        MDC.put("REQUEST_ID", requestId);
         filterChain.doFilter(request, response);
     }
 }
